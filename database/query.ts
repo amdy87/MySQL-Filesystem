84e9fb8ded2c110522174f1592262139b0325b59
Phoebe Schwartz <peschwartz@wisc.edu>
2024-03-03 20:03:39 -0600

    addUser, addDirectory, and addFile with preliminary tests

--- database/query.ts
+++ database/query.ts
@@ -1,9 +1,11 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, User } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-// creating a new user
-async function createUser(email: string, name: string, password: string) {
+// TODO: add user permissions for Directory and Files
+
+// adding a new user
+async function addUser(email: string, name: string, password: string) {
     try {
       const newUser = await prisma.user.create({
         data: {
@@ -12,19 +14,52 @@ async function createUser(email: string, name: string, password: string) {
           password,
         },
       });
-      console.log('User created:', newUser);
+      console.log('User added:', newUser);
       return newUser;
     } catch (error) {
-      console.error('Error creating user:', error);
+      console.error('Error adding user:', error);
       throw error;
     }
   }
   
   // creating a directory
-
+  async function addDirectory(directoryName: string, path: string, directoryParent: number) {
+    try {
+      const newDir = await prisma.directory.create({
+        data: {
+          directoryName,
+          path,
+          directoryParent,
+        },
+      });
+      console.log('New Directory created:', newDir);
+      return newDir;
+    } catch (error) {
+      console.error('Error creating directory:', error);
+      throw error;
+    }
+  }
 
   // creating a file
+  async function addFile(fileName: string, directoryParent: number, path: string) {
+    try {
+      const newFile = await prisma.file.create({
+        data: {
+          fileName,
+          directoryParent,
+          path,
+        },
+      });
+      console.log('New file created:', newFile);
+      return newFile;
+    } catch (error) {
+      console.error('Error creating file:', error);
+      throw error;
+    }
+  }
+  // reading contents of a directory
+
+  // reading contents of a file
 
 
-  
-  export { createUser };
+export {addUser, addDirectory, addFile}
--- database/tests.ts
+++ database/tests.ts
@@ -1,12 +1,20 @@
-import { createUser } from './query.ts'; // Assuming the file containing the createUser function is named userFunctions.js
+import { addUser, addDirectory, addFile } from './query.ts'; // Assuming the file containing the createUser function is named userFunctions.js
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 async function main() {
   try {
-    const newUser = await createUser('test@example.com', 'Test User', 'password123');
+    const newUser = await addUser('test@example5.com', 'Test User', 'password123');
     console.log('New user:', newUser);
+
+    // TODO: add user permission capabilities
+    const newDir = await addDirectory("directory_test", "./directory_test", 0);
+    console.log('New directory:', newDir);
+
+    const newFile = await addFile("file_test", newDir.id, "./directory_test/file_test");
+    console.log('New file:', newFile);
+
   } catch (error) {
     console.error('Error:', error);
   } finally {
