eb1a5f021f5f908525f5f329075759780b4aebd5
Jane Zhang <zhang2752@wisc.edu>
2024-04-03 00:02:43 -0500

    Remove recursive search in getDocsByParent

--- backend/src/utils/directory.ts
+++ backend/src/utils/directory.ts
@@ -9,6 +9,6 @@ import { Metadata } from './metadata';
 export interface DbDirectory extends DbFile {
   name: string;
   metadata: Metadata;
-  files: DbFile[];
-  directories: DbDirectory[];
+  files?: DbFile[];
+  directories?: DbDirectory[];
 }
